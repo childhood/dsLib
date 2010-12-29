36672dca07775b267b6db3c4aadc03b7572952be
naraing <naraing@hotmail.com>
2010-12-29 14:18:03 -0800

    Added heapsort driver.

--- tc_heap.c
+++ tc_heap.c
@@ -21,11 +21,48 @@
 #include <stdio.h>
 #include <heap.h>
 
-typedef struct
-   {
-      unsigned long num;
-      unsigned long idx;
+typedef struct {
+   unsigned long num;
+   unsigned long idx;
 } TC_HEAP_DATA_SET;
+
+void tc_heap_iterator (void)
+{
+   TC_HEAP_DATA_SET A[] = {{16, 0},
+                            {4, 0},
+                            {10, 0},
+                            {14, 0},
+                            {7, 0},
+                            {9, 0},
+                            {3, 0},
+                            {2, 0},
+                            {8, 0},
+                            {1, 0}};
+                            
+   /* TC_HEAP_DATA_SET A[] = {{16, 0}}; */
+   /* TC_HEAP_DATA_SET A[] = {}; */
+   
+   HEAP_T* h;
+   unsigned int length;
+   unsigned int idx;
+   void* p;
+   unsigned long key;
+   unsigned long ctx = 0;
+   
+   h = heap_create (DS_HEAP_MAX, 14);
+   length = sizeof (A)/sizeof (TC_HEAP_DATA_SET);
+   for (idx = 0; idx < length; idx++)
+   {
+      if (HEAP_ERR_ERR == heap_max_insert (h, A[idx].num, NULL, &A[idx].idx))
+         fprintf (stderr, "error adding heap element\n");
+   }
+   heap_graphviz_description (h, "heap-tc_heap_iterator.lst");
+
+   while (HEAP_ERR_ITERATOR_DONE != heap_iter (h, &p, &key, &ctx))
+   {
+      fprintf (stdout, "key = %lu\n", key);
+   }
+}
 
 /**
  * @brief test heap API
@@ -33,8 +70,6 @@ typedef struct
  */
 void tc_heap_max_main (void)
 {
-//  int A[] = {27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0};
-//   int A[] = {1,2,3};
    /*
    TC_HEAP_DATA_SET A[] = {{12,0},
                            {11,0},
@@ -73,30 +108,30 @@ void tc_heap_max_main (void)
    }
    //heap_build (h);
    heap_dump (h);
-   heap_graphviz_description (h, "heap-max-heap-i.lst");
+   heap_graphviz_description (h, "heap-tc_heap_max_main-i.lst");
    for (idx = 0; idx < length; idx++)
    {
       fprintf (stderr, "BEFORE -- A[idx].num = %lu A[idx].idx=%lu\n", A[idx].num, A[idx].idx);
    }
    
-   //heap_increase_key (h, A[9].idx, 216);
-   //A[9].num = 216;
+   heap_increase_key (h, A[9].idx, 216);
+   A[9].num = 216;
    
    //heap_increase_key (h, A[9].idx, 5);
- //A[9].num = 5;
-   heap_increase_key (h, A[0].idx, 20);
-	  A[0].num = 20;
+   //A[9].num = 5;
+   //heap_increase_key (h, A[0].idx, 20);
+//	  A[0].num = 20;
 
    heap_dump (h);
-   heap_graphviz_description (h, "heap-max-heap-ii.lst");
+   heap_graphviz_description (h, "heap-tc_heap_max_main-ii.lst");
 
-for (idx = 0; idx < length; idx++)
-{
-   fprintf (stderr, "AFTER -- A[idx].num = %lu A[idx].idx=%lu\n", A[idx].num, A[idx].idx);
-}
+   for (idx = 0; idx < length; idx++)
+   {
+      fprintf (stderr, "AFTER -- A[idx].num = %lu A[idx].idx=%lu\n", A[idx].num, A[idx].idx);
+   }
 
-return;
-
+   return;
+   
 }
 
 
@@ -106,8 +141,6 @@ return;
  */
 void tc_heap_min_main (void)
 {
-//  int A[] = {27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0};
-//   int A[] = {1,2,3};
    /*
    TC_HEAP_DATA_SET A[] = {{12,0},
                            {11,0},
@@ -131,7 +164,7 @@ void tc_heap_min_main (void)
                             {2, 0},
                             {8, 0}/*,
                             {1, 0}*/};
-  // int A[] = {4, 1, 3, 2, 16, 9, 10, 14, 8 ,7};
+
    HEAP_T* h;
    unsigned int length;
    unsigned int idx;
@@ -145,17 +178,17 @@ void tc_heap_min_main (void)
    }
    //heap_build (h);
    heap_dump (h);
-   heap_graphviz_description (h, "heap-min-heap-i.lst");
+   heap_graphviz_description (h, "heap-tc_heap_min_main-i.lst");
 
    for (idx = 0; idx < length; idx++)
    {
       fprintf (stderr, "BEFORE -- A[idx].num = %lu A[idx].idx=%lu\n", A[idx].num, A[idx].idx);
    }
 
-   heap_decrease_key (h, A[3].idx, 7); /* Changing the key value 16 to 1 */
-   A[3].num = 7;
+   heap_decrease_key (h, A[0].idx, 1); /* Changing the key value 16 to 1 */
+   A[0].num = 1;
    heap_dump (h);
-   heap_graphviz_description (h, "heap-min-heap-ii.lst");
+   heap_graphviz_description (h, "heap-tc_heap_min_main-ii.lst");
 
    for (idx = 0; idx < length; idx++)
    {
@@ -173,10 +206,6 @@ void tc_heap_min_main (void)
  */
 void tc_heap_min_ii_main (void)
 {
-/*  int A[] = {27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0};
- */
-/* int A[] = {1,2,3};
- */
 /*
    TC_HEAP_DATA_SET A[] = {{12,0},
                            {11,0},
@@ -200,7 +229,7 @@ void tc_heap_min_ii_main (void)
                             {2, 0},
                             {8, 0},
                             {1, 0}};
-  // int A[] = {4, 1, 3, 2, 16, 9, 10, 14, 8 ,7};
+
    HEAP_T* h;
    unsigned int length;
    unsigned int idx;
@@ -215,9 +244,8 @@ void tc_heap_min_ii_main (void)
       if (HEAP_ERR_ERR == heap_min_insert (h, A[idx].num, NULL, &A[idx].idx))
          fprintf (stderr, "error adding heap element\n");
    }
-   //heap_build (h);
    heap_dump (h);
-   heap_graphviz_description (h, "heap-min-heap-i.lst");
+   heap_graphviz_description (h, "heap-tc_heap_min_ii_main-i.lst");
 
    for (idx = 0; idx < length; idx++)
    {
@@ -228,7 +256,7 @@ void tc_heap_min_ii_main (void)
    while (HEAP_SIZE(h))
    {
       heap_extract_min (h, &p, &key);
-      snprintf (filename, 128, "heap-min-heap-%d.lst", idx);
+      snprintf (filename, 128, "heap-tc_heap_min_ii_main-%d.lst", idx);
       heap_graphviz_description (h, filename);
       fprintf (stderr, "FINAL == %lu\n", key);
       idx++;
