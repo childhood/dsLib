b7b6363bf98d243ff4e26e82c92b1b7f7f97faa5
naraing <naraing@hotmail.com>
2010-12-28 17:16:23 -0800

    Added tc_graphs.h; Added helper script to build graphviz graph PNG files.

---
+++ build_graphs.sh
@@ -0,0 +1,7 @@
+#!/bin/sh 
+dot -Tpng graph-i.lst > graph-i.png
+dot -Tpng graph-ii.lst > graph-ii.png
+dot -Tpng graph-iii.lst > graph-iii.png
+dot -Tpng graph-iv.lst > graph-iv.png
+dot -Tpng graph-v.lst > graph-v.png
+
